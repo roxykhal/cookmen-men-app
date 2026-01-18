0ab3721961034f9abe7493b7e537ec70b021fe85
Roxy <roxykhal@hotmail.com>
2026-01-18 14:09:05 +0000

    final commit

--- controllers/food.js
+++ controllers/food.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 // Point to the shared User model in ../models
 const User = require('../models/user.js');
+const methodOverride = require('method-override');
+
 
 router.get('/', async (req, res) => {
   try {
@@ -13,6 +15,7 @@ router.get('/', async (req, res) => {
 
     // Make pantry available to the view
     //assigning pantry to res.locals, means that the pantry array is accessible by res.locals and now views can also access it
+    //this is why we can use a loop on pantry in the index file. user.pantry is data from the DB
     res.locals.pantry = user.pantry;
 
     // Render the pantry index page
@@ -24,64 +27,96 @@ router.get('/', async (req, res) => {
   }
 });
 
-
+//Form to render a page that displays a form to add a new item to pantry
 router.get('/new', (req, res) => {
     res.render('foods/new');
 });
 
-//the create route
+//the create route POST, where the page goes after creating the new item 
 router.post('/', async (req, res) => {
-    try {
+  //When you have a POST route, this is for CREATE. Because Pantry is embedded into the user document, we need to push the array onto the user object. 
+  //req body holds the form data. So we need to first find the session id of the current logged in user, then push the pantry array into the body. Then save it
+  //Once down we redirect back to the foods page. 
 
-    //get the logged in users ID from the session, to look up that user in mongoDB
-    const userId = req.session.user._id;
-    //queries mongodb for the user document whose _id matches userId. We need the real
-    //database document to be able to modify the array so thats why we query mongoDB
-    const user = await User.findById(userId)
+  //req.session.user._id can query the DB. session.user shows who is just logged in but not in the Db
+  const currentUser = await User.findById(req.session.user._id);
+  currentUser.pantry.push(req.body);
 
-    if(!user) {
-        return res.redirect('/');
-    }
+  await currentUser.save();
+  res.redirect(`/users/${currentUser._id}/foods`)
 
-    //push new food object(req.body) to pantry array
-    user.pantry.push(req.body);
-    await user.save();
-
-    //for confirmation, redirect to pantry index
-res.redirect(`/users/${userId}/foods`);
-} catch (error) {
-    console.log(error);
-    res.redirect('/')
-}
 });
+
+
+router.get('/:itemId', async (req, res) => {
+
+  //want to find the logged in user so you can see what food they have in the pantry
+
+  const user = await User.findById(req.session.user._id);
+
+  //pantry is the name of the array subdocument, so we assign it to user to get the pantry ID for the user
+  //itemId is what is in the params and we assign it to the food variable so in the shows view we can view the name 
+  const food = user.pantry.id(req.params.itemId)
+
+  res.render('foods/show', { food })
+})
+
 
 //Delete foods
 
 router.delete('/:itemId', async (req, res) => {
   try {
-    const userId = req.session.user._id;
-    const user = await User.findById(userId);
+    
+    const user = await User.findById(req.session.user._id);
+
     if (!user) return res.redirect('/');
 
+    //pantry is embedded in user, so we have to access it like this
     const item = user.pantry.id(req.params.itemId);
+    //removes a subdocument
     if (item) {
       item.deleteOne(); // remove the matching subdocument from the array
+      //item is not top level, we need to save the user model to ensure the db update is done
       await user.save();
     }
 
-    res.redirect(`/users/${userId}/foods`);
+    res.redirect(`/users/${user._id}/foods`);
   } catch (error) {
     console.log(error);
     res.redirect('/');
   }
 });
 
-//When you click on the hyperlink of one of the food items. I want to be able to delete it
+router.get('/:itemId/edit', async (req, res) => {
 
-router.get('/:itemId', async (req, res) => {
-  const user = await User.findById(req.params.itemId);
+  const user = await User.findById(req.session.user._id);
 
-  res.render('foods/show')
+  if (!user) return res.redirect('/');
+
+  const food = user.pantry.id(req.params.itemId);
+  res.render('foods/edit', { food })
+}
+)
+
+router.put('/:itemId', async (req, res) => {
+  //find current logged in user
+  const user = await User.findByIdAndUpdate(req.session.user._id);
+  
+  //find the specific pantry item
+  const item = user.pantry.id(req.params.itemId);
+
+  if(!item) return res.redirect('/');
+
+  //update the subdocument using the form data, equivalent to item.name = req.body.name. This is where the update actually happens
+  //item.save changes the data in memory
+  item.set(req.body);
+  //persist those changes to the DB, subdocuments do not live in their own collection so the parent is saved
+  await user.save();
+
+  res.redirect(`/users/${user._id}/foods`);
+
+  
 })
 
 module.exports = router;
+
--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const isSignedIn = require('./middleware/is-signed-in.js');
 const passUserToView = require('./middleware/pass-user-to-view.js');
 const authController = require('./controllers/auth.js')
 const foodsController = require('./controllers/food.js');
+const methodOverride = require('method-override');
+
 
 //req.body will be undefined without this code (how express users client data from forms e.g. sign up form)
 app.use(express.urlencoded({ extended: true }));
@@ -24,6 +26,7 @@ app.set('view engine', 'ejs');
 //add auth controller before isSignedin to prevent the middleware running before the /auth routes
 //creates a redirect loop so the view never renders
 app.use('/auth', authController);
+app.use(methodOverride('_method'));
 app.use(isSignedIn);
 
 // Use relative paths so Node resolves local files correctly
